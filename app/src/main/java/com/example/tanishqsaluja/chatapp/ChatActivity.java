289ad0fda58f64aa8ed58ef13374703584e4cbc7
Tanishq Saluja <tanishqsaluja18@gmail.com>
2018-02-18 03:57:33 +0530

    chore : added and updated layout of chat

--- app/src/main/java/com/example/tanishqsaluja/chatapp/ChatActivity.java
+++ app/src/main/java/com/example/tanishqsaluja/chatapp/ChatActivity.java
@@ -1,6 +1,9 @@
 package com.example.tanishqsaluja.chatapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,9 +14,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -23,6 +29,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -41,6 +52,7 @@ public class ChatActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList<Message> arrayList;
     MessageAdapter messageAdapter;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,11 +65,13 @@ public class ChatActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(getIntent().getStringExtra("name"));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        storageReference = FirebaseStorage.getInstance().getReference();
+
         recyclerView = findViewById(R.id.rvmessages);
         recyclerView.setLayoutManager(new LinearLayoutManager(ChatActivity.this));
         recyclerView.setHasFixedSize(true);
         arrayList = new ArrayList<>();
-        messageAdapter = new MessageAdapter(arrayList);
+        messageAdapter = new MessageAdapter(ChatActivity.this, arrayList);
         recyclerView.setAdapter(messageAdapter);
 
         add = findViewById(R.id.add);
@@ -114,9 +128,116 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             /*   Intent gallaryIntent = new Intent();
+                gallaryIntent.setType("image*s");
+                gallaryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(gallaryIntent, "SELECT IMAGE"), 123);
+             */
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(ChatActivity.this);
+            }
+        });
+
         //function to retreive the messages
 
         loadmessages();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                final String current_user_ref = "/messages/" + currentUserID + "/" + friendUserID + "/";
+                final String friend_user = "/messages/" + friendUserID + "/" + currentUserID + "/";
+                DatabaseReference user_message_push = FirebaseDatabase.getInstance().getReference()
+                        .child(current_user_ref).child(friend_user).push();
+                final String push_id = user_message_push.getKey();
+
+                StorageReference filepath = storageReference.child("message_images").child(push_id + ".jpg");
+                Uri resultUri = result.getUri();
+
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        String download_url = task.getResult().getDownloadUrl().toString();
+                        Map messageMap = new HashMap();
+                        messageMap.put("message", download_url);
+                        messageMap.put("seen", false);
+                        messageMap.put("time", ServerValue.TIMESTAMP);
+                        messageMap.put("type", "image");
+                        messageMap.put("from", friendUserID);
+
+                        Map messageUserMap = new HashMap();
+                        messageUserMap.put(current_user_ref + push_id, messageMap);
+                        messageUserMap.put(friend_user + push_id, messageMap);
+
+                        entermessage.setText("");
+
+                        databaseReference = FirebaseDatabase.getInstance().getReference();
+                        databaseReference.updateChildren(messageUserMap, new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                                if (databaseError != null) {
+                                    Log.e("test", databaseError.getMessage());
+                                }
+                            }
+                        });
+
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+
+        }
+    /*    if (requestCode == 123 && requestCode == RESULT_OK) {
+            Uri imageuri = data.getData();
+            final String current_user_ref = "/messages/" + currentUserID + "/" + friendUserID + "/";
+            final String friend_user = "/messages/" + friendUserID + "/" + currentUserID + "/";
+            DatabaseReference user_message_push = FirebaseDatabase.getInstance().getReference()
+                    .child(current_user_ref).child(friend_user).push();
+            final String push_id = user_message_push.getKey();
+
+            StorageReference filepath = storageReference.child("message_images").child(push_id + ".jpg");
+            filepath.putFile(imageuri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    String download_url = task.getResult().getDownloadUrl().toString();
+                    Map messageMap = new HashMap();
+                    messageMap.put("message", download_url);
+                    messageMap.put("seen", false);
+                    messageMap.put("time", ServerValue.TIMESTAMP);
+                    messageMap.put("type", "image");
+                    messageMap.put("from", friendUserID);
+
+                    Map messageUserMap = new HashMap();
+                    messageUserMap.put(current_user_ref + push_id, messageMap);
+                    messageUserMap.put(friend_user + push_id, messageMap);
+
+                    entermessage.setText("");
+
+                    databaseReference = FirebaseDatabase.getInstance().getReference();
+                    databaseReference.updateChildren(messageUserMap, new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                            if (databaseError != null) {
+                                Log.e("test", databaseError.getMessage());
+                            }
+                        }
+                    });
+
+                }
+            });
+*/
 
     }
 
--- app/src/main/java/com/example/tanishqsaluja/chatapp/MessageAdapter.java
+++ app/src/main/java/com/example/tanishqsaluja/chatapp/MessageAdapter.java
@@ -1,15 +1,19 @@
 package com.example.tanishqsaluja.chatapp;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -22,17 +26,21 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     ArrayList<Message> arrayList;
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
+    Context context;
 
-    MessageAdapter(ArrayList<Message> list) {
+    MessageAdapter(Context c, ArrayList<Message> list) {
         this.arrayList = list;
+        this.context = c;
     }
 
     class MessageViewHolder extends RecyclerView.ViewHolder {
         TextView message;
+        ImageView imageView;
 
         public MessageViewHolder(View itemView) {
             super(itemView);
             message = itemView.findViewById(R.id.usermessage);
+            imageView = itemView.findViewById(R.id.imagemessage);
         }
     }
 
@@ -44,18 +52,25 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     @Override
     public void onBindViewHolder(MessageAdapter.MessageViewHolder holder, int position) {
         Message user = arrayList.get(position);
-        String current_user=user.getFrom();
+        String current_user = user.getFrom();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        Log.e("test",firebaseUser.getUid());
-        Log.e("test",current_user);
-        if (current_user.equals(firebaseUser.getUid())) {
-            holder.message.setBackgroundResource(R.drawable.message_text_background_other);
-            holder.message.setTextColor(Color.BLACK);
-        }else {
-            holder.message.setBackgroundResource(R.drawable.message_text_background);
-            holder.message.setTextColor(Color.WHITE);
+        Log.e("test", firebaseUser.getUid());
+        Log.e("test", current_user);
+        if (user.getType().equals("text")) {
+            holder.imageView.setVisibility(View.INVISIBLE);
+            if (current_user.equals(firebaseUser.getUid())) {
+                holder.message.setBackgroundResource(R.drawable.message_text_background_other);
+                holder.message.setTextColor(Color.BLACK);
+            } else {
+                holder.message.setBackgroundResource(R.drawable.message_text_background);
+                holder.message.setTextColor(Color.WHITE);
+            }
+            holder.message.setText(user.getMessage());
+        } else {
+
+            holder.message.setVisibility(View.INVISIBLE);
+            Picasso.with(context).load(user.getMessage()).placeholder(R.drawable.bot).into(holder.imageView);
         }
-        holder.message.setText(user.getMessage());
     }
 
     @Override
